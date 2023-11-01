c140bcdae50711db8316f5341e5d0264d55cfd70
Liana Lazareva <liana.lazareva@tktk.ee>
2023-11-01 17:37:33 +0200

    add Functions code

---
+++ Functions/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if(userChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodByu();
+}
+
+
+static void PrintHello() //function / method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodByu()
+{
+    Console.WriteLine("See you later, aligator. ");
+}
